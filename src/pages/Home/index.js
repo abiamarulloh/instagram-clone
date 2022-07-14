9f575827adb891c0eab45f0c1c3f0c4c3d2191be
abi amarulloh <abiamarulloh06@gmail.com>
2022-07-14 10:58:00 +0700

    update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,59 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import {colors} from '../../utils';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: {},
+    };
+  }
+
+  componentDidMount() {
+    this.getProduct();
+  }
+
+  getProduct = () => {
+    //GET request
+    fetch(
+      'https://raw.githubusercontent.com/abiamarulloh/simple-ecommerce/master/api/products.json?token=GHSAT0AAAAAABUZMOQWGNMWAWKRXCWHNGF6YWPRVZQ',
+      {
+        method: 'GET',
+      },
+    )
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+        this.setState({products: result});
+      })
+      .catch(function (error) {
+        console.log(
+          'There has been a problem with your fetch operation: ' +
+            error.message,
+        );
+        // ADD THIS THROW error
+        throw error;
+      });
+  };
+
   render() {
     return (
       <>
         <View style={styles.page}>
           <View style={styles.container}>
             <ScrollView>
-              <Text>Page Home</Text>
+              {/* {this.state.products.content.map(product => {
+                return (
+                  <>
+                    <View style={styles.productItem}>
+                      <Text>{product.title}</Text>
+                    </View>
+                  </>
+                );
+              })} */}
             </ScrollView>
           </View>
         </View>
